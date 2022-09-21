2843bdb28cf6d9e425b3ed8488a816a799c9a160
Nguyen Hoang Gia Bao <giabao@evol.vn>
2022-09-21 23:08:51 +0700

    Option 2: change typeof location from number to boolean

--- battleship.js
+++ battleship.js
@@ -1,12 +1,9 @@
-const location1 = Math.floor(Math.random() * 5);
-const location2 = location1 + 1;
-const location3 = location1 + 2;
+let location1 = Math.floor(Math.random() * 5);
+let location2 = location1 + 1;
+let location3 = location1 + 2;
 let guess;
 let totalGuess = 0;
 let hit = 0;
-let isHitLocation1 = false;
-let isHitLocation2 = false;
-let isHitLocation3 = false;
 let isSunk = false;
 
 alert("Let's play a game call Battle Ship!")
@@ -16,32 +13,21 @@ while (!isSunk) {
         guess = parseInt(prompt("Please enter a number between 0 and 6"));
     }
     totalGuess = totalGuess + 1;
-
     if (guess === location1) {
-        if (isHitLocation1) {
-            alert("You already hit this location!")
-        } else {
-            alert("Hit!")
-            isHitLocation1 = true;
-        }
+        alert("Hit!")
+        location1 = true;
     } else if (guess === location2) {
-        if (isHitLocation2) {
-            alert("You already hit this location!")
-        } else {
-            alert("Hit!")
-            isHitLocation2 = true;
-        }
+        alert("Hit!")
+        location2 = true;
     } else if (guess === location3) {
-        if (isHitLocation3) {
-            alert("You already hit this location!")
-        } else {
-            alert("Hit!")
-            isHitLocation3 = true;
-        }
+        alert("Hit!")
+        location3 = true;
     } else {
         alert("Miss!")
     }
-    isSunk = isHitLocation1 && isHitLocation2 && isHitLocation3
+    isSunk = (typeof location1 === 'boolean' ? location1 : false)
+        && (typeof location2 === 'boolean' ? location2 : false)
+        && (typeof location3 === 'boolean' ? location3 : false)
 }
 alert("Congratulations! You won!");
 console.log("You hit the ship with a number of guesses: ", totalGuess);
