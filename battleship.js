100f317baca605f8b6cd7260fc1676b8a0591714
Gia Bao Nguyen Hoang <giabao.nguyenhoang1001@gmail.com>
2024-11-13 10:48:04 +0700

    Add resetGame function to reset ship's location and remove .ship when reset the game

--- battleship.js
+++ battleship.js
@@ -1,16 +1,26 @@
-let location1 = Math.floor(Math.random() * 5);
-let location2 = location1 + 1;
-let location3 = location1 + 2;
+let location1;
+let location2;
+let location3;
+function resetGame() {
+  location1 = Math.floor(Math.random() * 5);
+  location2 = location1 + 1;
+  location3 = location1 + 2;
+  // Remove the ship from the game board
+  const shipLocations = document.querySelectorAll(".ship");
+  shipLocations.forEach((ship) => {
+    ship.classList.remove("ship");
+  });
+  // Display the ship on the game board
+  const shipLocation1 = document.querySelector(`[data-x="${location1}"]`);
+  const shipLocation2 = document.querySelector(`[data-x="${location2}"]`);
+  const shipLocation3 = document.querySelector(`[data-x="${location3}"]`);
+  console.log(shipLocation1, shipLocation2, shipLocation3);
+  [shipLocation1, shipLocation2, shipLocation3].forEach((ship) => {
+    ship.classList.add("ship");
+  });
+}
+resetGame();
 console.log(location1, location2, location3);
-
-// Display the ship on the game board
-const shipLocation1 = document.querySelector(`[data-x="${location1}"]`);
-const shipLocation2 = document.querySelector(`[data-x="${location2}"]`);
-const shipLocation3 = document.querySelector(`[data-x="${location3}"]`);
-console.log(shipLocation1, shipLocation2, shipLocation3);
-[shipLocation1, shipLocation2, shipLocation3].forEach((ship) => {
-  ship.classList.add("ship");
-});
 
 const startButton = document.querySelector(".start-button");
 startButton.addEventListener("click", () => {
@@ -20,20 +30,23 @@ startButton.addEventListener("click", () => {
   let hit = 0;
   let isSunk = false;
   while (!isSunk) {
-    guess = parseInt(prompt("Enter a guess number"));
+    guess = prompt("Enter a guess number");
+    console.log("Your guess is: ", guess);
     while (guess < 0 || guess > 6 || guess === "") {
-      guess = parseInt(prompt("Please enter a number between 0 and 6"));
+      guess = prompt("Please enter a number between 0 and 6");
     }
     totalGuess = totalGuess + 1;
-    if (guess === location1) {
+    if (parseInt(guess) === location1) {
       alert("Hit!");
       location1 = true;
-    } else if (guess === location2) {
+    } else if (parseInt(guess) === location2) {
       alert("Hit!");
       location2 = true;
-    } else if (guess === location3) {
+    } else if (parseInt(guess) === location3) {
       alert("Hit!");
       location3 = true;
+    } else if (guess === "q") {
+      break;
     } else {
       alert("Miss!");
     }
@@ -42,9 +55,9 @@ startButton.addEventListener("click", () => {
       (typeof location2 === "boolean" ? location2 : false) &&
       (typeof location3 === "boolean" ? location3 : false);
   }
-  alert("Congratulations! You won!");
+  if (isSunk) {
+    alert("Congratulations! You won!");
+  }
   console.log("You hit the ship with a number of guesses: ", totalGuess);
-  location1 = null;
-  location2 = null;
-  location3 = null;
+  resetGame();
 });
