b32d9b765757db2df0d861344e757d6b8bbfa0ee
Gia Bao Nguyen Hoang <giabao.nguyenhoang1001@gmail.com>
2024-11-17 19:28:19 +0700

    Add isNumberic function

--- battleship.js
+++ battleship.js
@@ -1,6 +1,7 @@
 let location1;
 let location2;
 let location3;
+
 function resetGame() {
   location1 = Math.floor(Math.random() * 5);
   location2 = location1 + 1;
@@ -14,15 +15,17 @@ function resetGame() {
   const shipLocation1 = document.querySelector(`[data-x="${location1}"]`);
   const shipLocation2 = document.querySelector(`[data-x="${location2}"]`);
   const shipLocation3 = document.querySelector(`[data-x="${location3}"]`);
-  console.log(shipLocation1, shipLocation2, shipLocation3);
   [shipLocation1, shipLocation2, shipLocation3].forEach((ship) => {
     ship.classList.add("ship");
   });
 }
-resetGame();
-console.log(location1, location2, location3);
+
+function isNumberic(str) {
+  return /^[0-9]+$/.test(str);
+}
 
 const startButton = document.querySelector(".start-button");
+resetGame();
 startButton.addEventListener("click", () => {
   alert("Let's play a game call Battle Ship!");
   let guess;
@@ -31,18 +34,13 @@ startButton.addEventListener("click", () => {
   let isSunk = false;
   while (!isSunk) {
     guess = prompt("Enter a guess number");
-    console.log("guess nè: ", guess);
-    console.log("parseInt(guess) nè: ", isNaN(parseInt(guess)));
+    console.log("guess === ", guess === "");
+    console.log("isNumberic(guess) nè: ", isNumberic(guess));
 
     // Validate the user input
     if (guess === "q" || guess === null) {
       break;
-    } else if (
-      guess < 0 ||
-      guess > 6 ||
-      guess === "" ||
-      isNaN(parseInt(guess))
-    ) {
+    } else if (guess < 0 || guess > 6 || guess === "" || !isNumberic(guess)) {
       alert("Please enter a number between 0 and 6");
     } else {
       totalGuess = totalGuess + 1;
@@ -61,8 +59,6 @@ startButton.addEventListener("click", () => {
         alert("Miss!");
       }
     }
-  }
-  if (isSunk) {
   }
   resetGame();
 });
